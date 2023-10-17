4088453dea4876d17c2b51ffa3948b5c0e2c635f
Kirat Bains <kiratbains09@gmail.com>
2023-10-16 22:44:26 -0700

    feat: strength meter

--- main.py
+++ main.py
@@ -23,6 +23,31 @@ def generate_password(length, use_digits=True, use_special=True):
     random.shuffle(password)
     return ''.join(password)
 
+def password_strength(password):
+    strength = 0
+    length = len(password)
+    has_upper = any(char.isupper() for char in password)
+    has_lower = any(char.islower() for char in password)
+    has_digit = any(char.isdigit() for char in password)
+    has_special = any(char in string.punctuation for char in password)
+
+    # Assess length and character types
+    if length >= 8:
+        strength += 2
+    elif length >= 6:
+        strength += 1
+
+    if has_upper:
+        strength += 1
+    if has_lower:
+        strength += 1
+    if has_digit:
+        strength += 1
+    if has_special:
+        strength += 2
+
+    return strength
+
 try:
     password_length = int(input("Enter the desired password length: "))
     use_digits = input("Use digits in the password? (yes/no): ").lower() == "yes"
@@ -30,6 +55,9 @@ try:
 
     password = generate_password(password_length, use_digits, use_special)
     print("Random Password:", password)
+
+    strength = password_strength(password)
+    print("Password Strength:", strength, "out of 10")
 except ValueError as e:
     print("Error:", e)
 except KeyboardInterrupt:
